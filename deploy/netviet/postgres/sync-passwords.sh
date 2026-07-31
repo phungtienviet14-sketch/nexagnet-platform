45639100bc4aa51adb8a5fcf5ead8ceb3e261162
DESKTOP-MK39ILP\SEATECH <hainv@seatechit.com.vn>
2026-07-31 12:12:42 +0700

    fix: bootstrap missing application database roles

--- deploy/netviet/postgres/sync-passwords.sh
+++ deploy/netviet/postgres/sync-passwords.sh
@@ -13,6 +13,22 @@ PGPASSWORD="${POSTGRES_PASSWORD}" psql \
   --set=flowise_password="${FLOWISE_DB_PASSWORD}" \
   --username "${POSTGRES_USER}" \
   --dbname postgres <<'EOSQL'
+SELECT format('CREATE ROLE zalo LOGIN PASSWORD %L', :'zalo_password')
+WHERE NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'zalo')
+\gexec
+
+SELECT format('CREATE ROLE flowise LOGIN PASSWORD %L', :'flowise_password')
+WHERE NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'flowise')
+\gexec
+
+SELECT 'CREATE DATABASE zalo OWNER zalo'
+WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'zalo')
+\gexec
+
+SELECT 'CREATE DATABASE flowise OWNER flowise'
+WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'flowise')
+\gexec
+
 ALTER ROLE zalo WITH PASSWORD :'zalo_password';
 ALTER ROLE flowise WITH PASSWORD :'flowise_password';
 EOSQL
